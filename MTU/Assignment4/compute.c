4b7d1d2f2b54e08fd0577ebc86d6492471dac241
Koko <koko.nanahji@gmail.com>
2018-12-29 05:16:02 +0000

    fixed Assignment 2

--- MTU/Assignment4/compute.c
+++ MTU/Assignment4/compute.c
@@ -1,51 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include  <sys/types.h>
 #include  <sys/ipc.h>
 #include  <sys/shm.h>
 
 
-
 int main( int argc, char *argv[] )  {
+	int ans =0 ; 		// result of the multiplication
+	int i;				// integer for loop
 
-	key_t key;
-	int       shm_id;        /* shared memory ID      */   	
-	int       *shm_ptr;
-
-	int m = atoi(argv[1]);
-	int n = atoi(argv[2]);
-
-	int L = atoi(argv[3]);
-	int M = atoi(argv[4]);
-	int N = atoi(argv[5]);
-
-	printf("   ### PROC(%d): entering with row %d and column %d\n",getpid(),m,n-L);
-	key = ftok(".", 'x');
-	shm_id = shmget(key, 100 * sizeof(int), 0666);
+	key_t key;					// key for the shared memory
+	int       shm_id;        	// shared memory ID      	
+	int       *shm_ptr;			// pointer to the shared memory 
 	
+	int row = atoi(argv[1]);			// row to multiply
+	int column = atoi(argv[2]);			// column to multiply
+	int L = atoi(argv[3]);				// number of rows of the first matrix
+	int M = atoi(argv[4]);				// number of rows of the second matrix (columns of first matrix)
+	int N = atoi(argv[5]);				// number of columns of the second matrix
+
+	printf("   ### PROC(%d): entering with row %d and column %d\n",getpid(),row,column-L);
+	
+	key = ftok(".",'x');		// generate the key with the same parameters as the parent
+	//	get the id of the shared with the specified key (shared memory allocated by the parent process with the same key)
+	shm_id = shmget(key, 128 * sizeof(int), 0666);	
 	if (shm_id < 0) {
 		printf("*** shmget error (client) ***\n");
 		exit(1);
 	}
-//	printf("   Client has received a shared...\n");
 
+	//	attach to shared memory
 	shm_ptr =  (int *) shmat(shm_id, NULL, 0);  /* attach */
-	if ((int) shm_ptr == -1) {
+	if ( *shm_ptr == -1) {
 		printf("*** shmat error (client) ***\n");
 		exit(1);
 	}
-//	printf("   Client has attached the shared memory...\n");
 
-	int ans =0 ; 	
-	int i;
+	// calculate the by multiplying the mth row and nth column from shared memory
 	for (i=0;i<M;i++){
-		ans += ( (*(shm_ptr + i + (M * m)))  *  (*(shm_ptr + i +  (M * n)))  );
+		ans += ( (*(shm_ptr + i + (M * row)))  *  (*(shm_ptr + i +  (M * column)))  );
 	}	
+	printf("   ### PROC(%d): C[%d][%d]=%d\n",getpid(),row,column-L,ans);
 
-	printf("   ### PROC(%d): C[%d][%d]=%d\n",getpid(),m,n-L,ans);
-	*(shm_ptr + (L*M*M*N) + ( (m * N) + (n-L) )  ) = ans;
+	// save the result to shared memory
+	*(shm_ptr + (L*M*M*N) + ( (row * N) + (column-L) )  ) = ans;
+	
+	//	detach from shared memory
 	shmdt((void *) shm_ptr);
-//	printf("   Client has detached its shared memory...\n");
-//	printf("   Client exits...\n");
+
 	return 0;
 }
--- MTU/Assignment4/main.c
+++ MTU/Assignment4/main.c
@@ -1,155 +1,185 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include  <sys/types.h>
 #include  <sys/ipc.h>
 #include  <sys/shm.h>
+#include <sys/wait.h>
+
+#define INPUT_FILE_NAME "data2.in"
+#define MATRIX_MULT "./compute"
 
 
+// @brief gets a file pointer and returns the size of the file 
+// @param fp is a pointer to a file
+// @return long that represents the number of elements in the file
+long fsize(FILE *fp){
+    long prev=ftell(fp);
+    fseek(fp, 0L, SEEK_END);
+    long sz=ftell(fp);
+    fseek(fp,prev,SEEK_SET); //go back to where we were
+    return sz;
+}
 
 int main() {
-	key_t key;
-	int       shm_id;        /* shared memory ID      */   	
-	int       *shm_ptr;
+	///////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////  Set Up  //////////////////////////////////////////
+	// local variables
+	FILE *fp;
+	int i,j,k;									// integer for loops
+	int d;										// integer for values read
+	int l,m,m1,n;								// dimensions of the matrices
+   	long size;									// size neccessary for shared memory
 
-	key = ftok(".",'x');
+   	fp = fopen (INPUT_FILE_NAME,"r");			// open the file for reading
+	if (fp == NULL) {
+		printf ("Input file not created");
+		return 1;
+	}
+	// get current file size to allocate memory with necessary size and duplicate that in order to save the results as well
+	size = 2 * fsize(fp); 							
+    printf("*** SIZE = %d\n",size);
 
-	shm_id = shmget(key, 100 * sizeof(int), IPC_CREAT | 0666);
+	key_t key;					// key for the shared memory
+	int       shm_id;        	// shared memory ID      	
+	int       *shm_ptr;			// pointer to the shared memory 
+
+	key = ftok(".",'x');		// generate the key (the other process must use the pass the same parameters to ftok )
+	// create the shared memory with read and write permissions and with size matching the input file size
+	shm_id = shmget(key,  size * sizeof(int), IPC_CREAT | 0666); 
 	if (shm_id < 0) {
 	     printf("shmget error\n");
 	     exit(1);
 	}
+
 	printf("Matrix multiplication with multiple processes:\n");
 	printf("\n");
         printf("*** MAIN: shared memory key = %d\n",key);
-        printf("*** MAIN: Main has received a shared memory of 100 integers\n");
+        printf("*** MAIN: Main has received a shared memory of %ld integers\n",size);
  
-
-	shm_ptr = (int *) shmat(shm_id, NULL, 0);  /* attach */
-	if ((int) shm_ptr == -1) {
+ 	// attach to the created shared memory	 
+	shm_ptr = (int *) shmat(shm_id, NULL, 0); 
+	if ( *shm_ptr == -1) {
 	     printf("*** shmat error (server) ***\n");
 	     exit(1);
 	}
-	printf("*** MAIN: shared memory attached and is ready to use\n");
+	printf("*** MAIN: shared memory attached and is ready to use\n\n\n");
 
-	printf("\n");
-	printf("\n");
-	///////////////////////////////////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////
-	FILE *fp;
-	fp = fopen ("data.in","r");
-	if (fp == NULL) {
-	printf ("File not created okay");
-	return 1;
-	}
 
-	int i,j;
-	int d;	
-	int l,m,m1,n;
-	fscanf(fp,"%d", &l);
-	fscanf(fp,"%d", &m);
+
+	///////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////// Read the input ///////////////////////////////////////
+
+	fscanf(fp,"%d", &l);						//	read the number of rows of the first matrix
+	fscanf(fp,"%d", &m);						//	read the number of columns of the first matrix
 
 	printf("Matrix A: %d rows and %d columns\n",l,m);
-
+	// read the elements from the file to shared memory
 	for (i=0;i<l;i++){
 		for (j=0;j<m;j++){
 			fscanf(fp,"%d", &d);
 			*(shm_ptr + (j + (i*m))) = d;
-//			printf("   %2d", (j + (i*m)));
 			printf("   %2d", d);
 		}
 			printf("\n");
 	}	
 	
-	fscanf(fp,"%d", &m1);
-	fscanf(fp,"%d", &n);
+	fscanf(fp,"%d", &m1);						//	read the number of rows of the second matrix
+	fscanf(fp,"%d", &n);						//	read the number of columns of the second matrix
 
 	printf("Matrix B: %d rows and %d columns\n",m1,n);
+	// read the elements from file to shared memory while transpoing it (save the transpose of the matrix)
 	for (i=0;i<m1;i++){
 		for (j=0;j<n;j++){
 			fscanf(fp,"%d", &d);
 			*(shm_ptr + (i + (j*m1))+ (l*m1) ) = d;
-//			printf("   %2d", (i + (j*m1))+ (l*m1));
 			printf("   %2d", d);
 		}
 			printf("\n");
 	}	
-
-
+	printf("\n");
+	
+	// close file descriptor 
+	fclose (fp);	
 	///////////////////////////////////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////// Multiplication /////////////////////////////////////
 
-
-//	for (i=0;i<l;i++){
-//		for (j=0;j<m;j++){
-//			printf("   %2d", *(shm_ptr + (j + (i*m))));
-//		}
-//			printf("\n");
-//	}
-		printf("\n");
-//	for (i=0;i<m;i++){
-//		for (j=0;j<n;j++){
-//			printf("   %2d", *(shm_ptr + (j  + (i*n))+ (l*m)));
-//		}
-//			printf("\n");
-//	}	
-
+	// proceed with multiplication if the number of columns of the first matrix matches the number of rows of the seconds matrix 
 	if (m1 == m){
 		pid_t  pid;
 		int    status;
-	
+		
+		// arguments to be passed to the child process
+		char ** arguments  = (char **) malloc(sizeof(char*) * 7);
+		arguments[0] = MATRIX_MULT;				// name of the child process to be called 
+		arguments[1] = (char *)malloc(sizeof(char) * 5);
+		arguments[2] = (char *)malloc(sizeof(char) * 5);
+		arguments[3] = (char *)malloc(sizeof(char) * 5);
+		arguments[4] = (char *)malloc(sizeof(char) * 5);
+		arguments[5] = (char *)malloc(sizeof(char) * 5);
+		sprintf(arguments[3], "%d", l);			// number of rows of the first matrix
+		sprintf(arguments[4], "%d", m);			// number of rows of the second matrix (columns of first matrix)
+		sprintf(arguments[5], "%d", n);			// number of columns of the second matrix
+		arguments[6] = NULL; 
+
 		printf("*** MAIN: about to spawn processes\n");
 		for(i=0;i<l;i++){
 			for(j=0;j<n;j++){
-				char *argv[7]; // reduce
-				argv[0] = "./compute";
-				argv[1] = (char *)malloc(sizeof(char) * 5);
-				argv[2] = (char *)malloc(sizeof(char) * 5);
-				argv[3] = (char *)malloc(sizeof(char) * 5);
-				argv[4] = (char *)malloc(sizeof(char) * 5);
-				argv[5] = (char *)malloc(sizeof(char) * 5);
-				sprintf(argv[1], "%d", i);
-				sprintf(argv[2], "%d",(j+l));
-				sprintf(argv[3], "%d", l);
-				sprintf(argv[4], "%d", m);
-				sprintf(argv[5], "%d", n);
-				argv[6] = NULL; // delete
-				if ((pid = fork()) < 0) {     /* fork a child process           */
+				sprintf(arguments[1], "%d", i);			//	row that the child is responsible to multiply
+				sprintf(arguments[2], "%d",(j+l));		//	column that the child is responsible to multiply
+
+				if ((pid = fork()) < 0) {    			 // fork child process
 					printf("*** ERROR: forking child process failed\n");
 					exit(1);
 				}
-			     	else if (pid == 0) {          /* for the child process:         */
-					if (execvp(*argv, argv) < 0) {     /* execute the command  */
+				else if (pid == 0) {          			// in the child process:        
+					if (execvp(*arguments, arguments) < 0) {     	// execute the command  
 						printf("*** ERROR: exec failed\n");
 						exit(1);
 					}
-				}
+				} 
 			}
 		}
 
+		// clean up the allocated memory
+		for (k=1;k<6;k++){
+			free(arguments[k]);
+		}
+		free(arguments);
+
+		// wait for the child processes to finish
 		for(i=0; i< n*l; i++){
 			wait(NULL);
 		}	
-			printf("\n");
+		printf("\n");
 
 
-
+		////////////////////////////////// Result ///////////////////////////////////////////
+		// Print the result of the multiplication by reading from the shared memory
 		printf("Matrix C: %d rows and %d columns\n",l,n);
 		for (i=0;i<l;i++){
 			for (j=0;j<n;j++){
-				printf("   %2d", *(shm_ptr + (l*m*m*n) + (j + (i*n))));
+				printf("   %6d", *(shm_ptr + (l*m*m*n) + (j + (i*n))));
 			}
 				printf("\n");
 		}
+
 	} else {
 		printf("*** MAIN: number of columns of matrix A is not equal to number of rows of matrix B \n");			
 		printf("*** MAIN: Input matrices can not be multiplied\n");
 	}
+
+
+	///////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////// Clean Up ///////////////////////////////////////////
+
+	// detach shared memory
 	shmdt(shm_ptr);
 	printf("Main has detached its shared memory...\n");
+	// remove the share memory
 	shmctl(shm_id, IPC_RMID, NULL); 
 	printf("Main has removed its shared memory...\n");
-  
-	fclose (fp);	
 	printf("Main exits...\n");
+
 	return 0;
 }
